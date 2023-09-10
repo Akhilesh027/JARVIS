26968dfdf26ff23c2444febf43ea0b3d8189d98a
Akhilesh027 <akhileshreddy027@gmail.com>
2023-09-10 23:56:28 +0530

    commands changed

--- app.js
+++ app.js
@@ -24,9 +24,9 @@ function greating(){
     }
  }
 window.addEventListener('load', ()=>{
-    speak("jarvis instalizing...");
-    speak("jarvis installed successfully")
-    speak("wellcome to javis world")
+    speak("friday  instalizing...");
+    speak("friday  installed successfully")
+    speak("wellcome to friday world")
     greating();
 })
 
@@ -49,61 +49,65 @@ function speakThis(message) {
 
     speech.text = "I did not understand what you said please try again";
 
-    if(message.includes('hey') || message.includes('hello')) {
+    if(message.includes('hey friday') || message.includes('hello friday')) {
         const finalText = "Hello Boss";
         speech.text = finalText;
     }
 
-    else if(message.includes('how are you')) {
+    else if(message.includes('friday how are you')) {
         const finalText = "I am fine boss tell me how can i help you";
         speech.text = finalText;
     }
-    else if(message.includes("who invented you ") || message.includes("who created you")){
+    else if(message.includes("friday who invented you") || message.includes("friday who created you")){
         const finaltext = "i was invented my verry talanted mr.Akhileshreddy he my boss and my god..."
         speech.text = finaltext;
     }
-    else if(message.includes('what is your name')) {
-        const finalText = "My name is jarvis";
+    else if(message.includes('whats is your name')) {
+        const finalText = "My name is friday ";
         speech.text = finalText;
     }
-
-    else if(message.includes('open google')) {
+    else if(message.includes("friday open gmail") || message.includes("open gmail")){
+        window.open("https://gmail.com", "_blank");
+        const finalText = "cheaking gmails"
+        speech.text = finalText;
+    }
+    else if(message.includes('friday open google') || message.includes("open google")) {
         window.open("https://google.com", "_blank");
         const finalText = "Opening Google";
         speech.text = finalText;
     }
 
-    else if(message.includes('open instagram')) {
+    else if(message.includes('friday open instagram') || message.includes("open instagram")) {
         window.open("https://instagram.com", "_blank");
         const finalText = "Opening instagram";
         speech.text = finalText;
     }
-    else if(message.includes('open youtube')) {
+    else if(message.includes('friday open youtube') || message.includes("open youtude")) {
         window.open("https://youtube.com", "_blank");
         const finaltext = "opening youtube";
         speech.text = finaltext;
     }
-    else if(message.includes('open chat gpt') || message.includes("open chatgpt")) {
+    else if(message.includes('friday open chat gpt') || message.includes("open chatgpt")) {
         window.open("https://chat.openai.com/", "_blank");
         const finaltext = "opening chatgpt";
         speech.text = finaltext;
     }
-    else if(message.includes('open my portfolio')){
+    else if(message.includes('friday open my website')){
         window.open("https://akhilesh027.github.io/Akhileshreddy-protofilo/", "_block");
         const finalText = "opening your protofilo bro";
         speech.text = finalText;
     }
-    else if(message.includes("open whatsapp")){
+    else if(message.includes("friday open whatsapp")){
         window.open('https://web.whatsapp.com/', "_blank");
         const finalText = "opening whatsapp bro"
         speech.text = finalText;
     }
-    else if(message.includes('what is') || message.includes('who is') || message.includes('what are')) {
+    else if(message.includes('friday what is') || message.includes('friday  who is') || message.includes('friday  what are')) {
         window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
         const finalText = "This is what i found on internet regarding " + message;
         speech.text = finalText;
     }
-    else if(message.includes('play') || message.includes('play music') || message.includes("in youtube")) {
+    else if(message.includes('friday play') || message.includes("...in youtube")) {
         window.open(`https://www.youtube.com/search?q=${message.replace(" ", "+")}`, "_blank");
         const finalText = "This is what i found on youtube regarding " + message;
         speech.text = finalText;
@@ -133,10 +137,7 @@ function speakThis(message) {
         speech.text = finalText;
     }
 
-    else {
-        
-    }
-
+    
     speech.volume = 1;
     speech.pitch = 1;
     speech.rate = 1;
